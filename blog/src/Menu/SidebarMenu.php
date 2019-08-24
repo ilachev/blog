43287dde6069c9c985466f8a7a0ed43d0b0451c7
imp-f <ilyakarachov@gmail.com>
2019-08-24 17:20:27 +0300

    Added user

---
+++ blog/src/Menu/SidebarMenu.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class SidebarMenu
+{
+    private $factory;
+    private $auth;
+
+    public function __construct(FactoryInterface $factory, AuthorizationCheckerInterface $auth)
+    {
+        $this->factory = $factory;
+        $this->auth = $auth;
+    }
+
+    public function build(): ItemInterface
+    {
+        $menu = $this->factory->createItem('root')
+            ->setChildrenAttributes(['class' => 'nav']);
+
+        $menu->addChild('Dashboard', ['route' => 'home'])
+            ->setExtra('icon', 'nav-icon icon-speedometer')
+            ->setAttribute('class', 'nav-item')
+            ->setLinkAttribute('class', 'nav-link');
+
+        if ($this->auth->isGranted('ROLE_MANAGE_USERS')) {
+            $menu->addChild('Users', ['route' => 'users'])
+                ->setExtra('icon', 'nav-icon icon-people')
+                ->setExtra('routes', [
+                    ['route' => 'users'],
+                    ['pattern' => '/^users\..+/']
+                ])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
+        }
+
+        return $menu;
+    }
+}
